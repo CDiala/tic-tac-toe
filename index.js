b53b8a3870f3de615e7162c28fac56453b09b37b
CDiala <dialachibuzo@yahoo.com>
2022-09-23 07:09:16 +0100

    Added more functions

--- index.js
+++ index.js
@@ -3,17 +3,35 @@ let player = document.querySelector(".player");
 let winnerLabel = document.querySelector(".winner");
 let gameTiles = document.querySelectorAll(".play-box");
 let winningArray = ["123", "456", "789", "147", "258", "369", "159", "357"];
+let xCount = 0;
+let oCount = 0;
+let xIndices = "";
+let oIndices = "";
 
 gameTiles.forEach((tile) => {
   tile.addEventListener("click", () => {
     markTile(tile);
-    getWinner();
+    countPlay();
+    setPlayIndex(tile.dataset.id);
+    getWinner(playerStart === "X" ? xCount : oCount);
     setPlayer();
   });
 });
 
 function setPlayer() {
-  playerStart = playerStart === "X" ? "O" : "X";
+  if (playerStart === "X") {
+    playerStart = "O";
+  } else {
+    playerStart = "X";
+  }
+}
+
+function countPlay() {
+  if (playerStart === "X") {
+    xCount++;
+  } else {
+    oCount++;
+  }
 }
 
 function markTile(tile) {
@@ -22,6 +40,20 @@ function markTile(tile) {
   }
 }
 
-function getWinner() {
-  let objWin = {};
+function setPlayIndex(num) {
+  if (playerStart === "X") {
+    xIndices += num.toString();
+    console.log(xIndices);
+  } else {
+    oIndices += num;
+    console.log(oIndices);
+  }
+}
+
+function getWinner(playCount) {
+  console.log(playerStart, playCount);
+  if (playCount < 3) {
+  } else {
+    console.log(playCount + " is up to 3");
+  }
 }
